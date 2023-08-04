52517b87725b85adee7656b1789a735c1be64e75
Ajay Titus <ajaytitus1386@gmail.com>
2023-08-05 01:26:16 +0530

    feat: add routes and constants for user rating

--- constants/routes/index.ts
+++ constants/routes/index.ts
@@ -1,8 +1,12 @@
 // Base Route names
 export const ROOM_NAME = "/room_name"
 
+export const USER_RATING = "/user_rating"
+
 // Functions for endpoints
 const userRoomNameRoute = (path: string) => `${ROOM_NAME}${path}`
+
+const userRatingRoute = (path: string) => `${USER_RATING}${path}`
 
 // Objects for endpoints - For Fronted
 export const ROOM_NAME_ROUTER = {
@@ -10,4 +14,8 @@ export const ROOM_NAME_ROUTER = {
         userRoomNameRoute(
             "/random" + (query.seed ? `?seed=${query.seed}` : "")
         ),
+}
+
+export const USER_RATING_ROUTER = {
+    NEW: userRatingRoute("/new"),
 }
